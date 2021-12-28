4764e077bbf902a442bb369fcd609efeda096795
Nathan Hayoun <nathan.hayoun@outlook.fr>
2021-12-28 21:55:07 +0100

    create class, add lib sql and test api for train

--- sntp-resa/src/main/java/fr/miage/m1/sntp/models/Ticket.java
+++ sntp-resa/src/main/java/fr/miage/m1/sntp/models/Ticket.java
@@ -1,43 +1,71 @@
 package fr.miage.m1.sntp.models;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 @Entity
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 public class Ticket {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id_ticket", nullable = false)
-    private Long idTicket;
+    private Long id;
 
-    private String gareDepart;
-    private String gareArrivee;
+    @Column(name = "DTYPE", nullable = false, length = 31)
+    private String dtype;
+
+    @Column(name = "dateDepart")
     private LocalDate dateDepart;
-    private LocalTime heureDepart;
-    private LocalTime heureArrivee;
-    private int numeroEtape;
 
-    public Ticket(String gareDepart, String gareArrivee, LocalDate dateDepart, LocalTime heureDepart, LocalTime heureArrivee, int numeroEtape) {
-        this.gareDepart = gareDepart;
-        this.gareArrivee = gareArrivee;
-        this.dateDepart = dateDepart;
-        this.heureDepart = heureDepart;
-        this.heureArrivee = heureArrivee;
+    @Column(name = "gareArrivee")
+    private String gareArrivee;
+
+    @Column(name = "gareDepart")
+    private String gareDepart;
+
+    @Column(name = "heureArrivee")
+    private LocalTime heureArrivee;
+
+    @Column(name = "heureDepart")
+    private LocalTime heureDepart;
+
+    @Column(name = "numeroEtape", nullable = false)
+    private Integer numeroEtape;
+
+    @Column(name = "place")
+    private Integer place;
+
+    public Integer getPlace() {
+        return place;
+    }
+
+    public void setPlace(Integer place) {
+        this.place = place;
+    }
+
+    public Integer getNumeroEtape() {
+        return numeroEtape;
+    }
+
+    public void setNumeroEtape(Integer numeroEtape) {
         this.numeroEtape = numeroEtape;
     }
 
-    public Ticket() {
-        
+    public LocalTime getHeureDepart() {
+        return heureDepart;
     }
 
-    public Long getIdTicket() {
-        return idTicket;
+    public void setHeureDepart(LocalTime heureDepart) {
+        this.heureDepart = heureDepart;
     }
 
-    public void setIdTicket(Long idTicket) {
-        this.idTicket = idTicket;
+    public LocalTime getHeureArrivee() {
+        return heureArrivee;
+    }
+
+    public void setHeureArrivee(LocalTime heureArrivee) {
+        this.heureArrivee = heureArrivee;
     }
 
     public String getGareDepart() {
@@ -64,27 +92,19 @@ public class Ticket {
         this.dateDepart = dateDepart;
     }
 
-    public LocalTime getHeureDepart() {
-        return heureDepart;
+    public String getDtype() {
+        return dtype;
     }
 
-    public void setHeureDepart(LocalTime heureDepart) {
-        this.heureDepart = heureDepart;
+    public void setDtype(String dtype) {
+        this.dtype = dtype;
     }
 
-    public LocalTime getHeureArrivee() {
-        return heureArrivee;
+    public Long getId() {
+        return id;
     }
 
-    public void setHeureArrivee(LocalTime heureArrivee) {
-        this.heureArrivee = heureArrivee;
-    }
-
-    public int getNumeroEtape() {
-        return numeroEtape;
-    }
-
-    public void setNumeroEtape(int numeroEtape) {
-        this.numeroEtape = numeroEtape;
+    public void setId(Long id) {
+        this.id = id;
     }
 }
