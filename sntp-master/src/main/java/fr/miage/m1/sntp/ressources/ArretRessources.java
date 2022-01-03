9c1cdf8ae7b505f251860f42bc8fbf3b2daeed2d
Etan Guir <etan.guir@gmail.com>
2022-01-03 21:51:04 +0100

    New: Add ressource for "Gare", "Itineraire", "Arret", "Passage", "LigneDeTrain

---
+++ sntp-master/src/main/java/fr/miage/m1/sntp/ressources/ArretRessources.java
@@ -0,0 +1,36 @@
+package fr.miage.m1.sntp.ressources;
+
+import fr.miage.m1.sntp.dao.ArretDAO;
+import fr.miage.m1.sntp.exceptions.ArretException;
+import fr.miage.m1.sntp.models.Arret;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@Path("/arrets")
+public class ArretRessources {
+    @Inject
+    ArretDAO arretDAO;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Arret> getArrets() {
+        return arretDAO.getAllArret();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/arret/{id}")
+    public Arret getArret(@PathParam("id") long id) throws ArretException {
+        try {
+            return arretDAO.findArret(id);
+        } catch (ArretException arretException) {
+            return null;
+        }
+    }
+}
