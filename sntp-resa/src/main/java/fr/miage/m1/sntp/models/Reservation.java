31b93df8a9697dd85172584fedbd738d21f81e72
aichanur <aichanur@hotmail.com>
2022-01-03 21:22:13 +0100

    finish schema resa

--- sntp-resa/src/main/java/fr/miage/m1/sntp/models/Reservation.java
+++ sntp-resa/src/main/java/fr/miage/m1/sntp/models/Reservation.java
@@ -3,45 +3,35 @@ package fr.miage.m1.sntp.models;
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.Set;
 
 @Entity
 public class Reservation {
     @Id
-
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_reservation", nullable = false)
     private Long id;
 
     @Column(name = "dateDeReservation")
     private LocalDate dateDeReservation;
 
-    @Column(name = "dateDeVoyage")
-    private LocalDate dateDeVoyage;
-
-    @Column(name = "gareArrivee")
-    private String gareArrivee;
-
-    @Column(name = "gareDepart")
-    private String gareDepart;
-
-    @Column(name = "heureArrivee")
-    private LocalTime heureArrivee;
-
-    @Column(name = "heureDeDepart")
-    private LocalTime heureDeDepart;
 
     @Column(name = "prix", nullable = false)
     private Double prix;
 
     @ManyToOne
-    @JoinColumn(name = "voyageur_id_voyageur")
-    private fr.miage.m1.sntp.models.Voyageur voyageurIdVoyageur;
+    @JoinColumn(name = "id_voyageur")
+    private Voyageur voyageurConcernee;
 
-    public fr.miage.m1.sntp.models.Voyageur getVoyageurIdVoyageur() {
-        return voyageurIdVoyageur;
+    @OneToMany(mappedBy="reservationConcernee",fetch = FetchType.EAGER)
+    private Set<Ticket> tickets ;
+
+    public Voyageur getVoyageur() {
+        return voyageurConcernee;
     }
 
-    public void setVoyageurIdVoyageur(fr.miage.m1.sntp.models.Voyageur voyageurIdVoyageur) {
-        this.voyageurIdVoyageur = voyageurIdVoyageur;
+    public void setVoyageur(Voyageur voyageurConcernee) {
+        this.voyageurConcernee = voyageurConcernee;
     }
 
     public Double getPrix() {
@@ -50,46 +40,6 @@ public class Reservation {
 
     public void setPrix(Double prix) {
         this.prix = prix;
-    }
-
-    public LocalTime getHeureDeDepart() {
-        return heureDeDepart;
-    }
-
-    public void setHeureDeDepart(LocalTime heureDeDepart) {
-        this.heureDeDepart = heureDeDepart;
-    }
-
-    public LocalTime getHeureArrivee() {
-        return heureArrivee;
-    }
-
-    public void setHeureArrivee(LocalTime heureArrivee) {
-        this.heureArrivee = heureArrivee;
-    }
-
-    public String getGareDepart() {
-        return gareDepart;
-    }
-
-    public void setGareDepart(String gareDepart) {
-        this.gareDepart = gareDepart;
-    }
-
-    public String getGareArrivee() {
-        return gareArrivee;
-    }
-
-    public void setGareArrivee(String gareArrivee) {
-        this.gareArrivee = gareArrivee;
-    }
-
-    public LocalDate getDateDeVoyage() {
-        return dateDeVoyage;
-    }
-
-    public void setDateDeVoyage(LocalDate dateDeVoyage) {
-        this.dateDeVoyage = dateDeVoyage;
     }
 
     public LocalDate getDateDeReservation() {
@@ -106,5 +56,13 @@ public class Reservation {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public Set<Ticket> getTickets() {
+        return tickets;
+    }
+
+    public void setTickets(Set<Ticket> tickets) {
+        this.tickets = tickets;
     }
 }
