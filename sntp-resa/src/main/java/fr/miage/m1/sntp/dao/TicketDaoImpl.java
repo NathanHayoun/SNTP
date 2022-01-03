2c0a074cfd09f6e5710b28e4b7cbedc14f764e0f
Raphael Trabelsi <rt.pro26@gmail.com>
2022-01-03 23:50:39 +0100

    finish resa dao

---
+++ sntp-resa/src/main/java/fr/miage/m1/sntp/dao/TicketDaoImpl.java
@@ -0,0 +1,37 @@
+package fr.miage.m1.sntp.dao;
+
+import fr.miage.m1.sntp.models.Ticket;
+import fr.miage.m1.sntp.utils.LibSQL;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+public class TicketDaoImpl implements TicketDao {
+    @PersistenceContext
+    EntityManager entityManager;
+    @Override
+    public List<Ticket> findAll() {
+        return LibSQL.findAll(entityManager, Ticket.class);
+    }
+
+    @Override
+    public Ticket findById(int id) {
+        return LibSQL.findObject(entityManager,Ticket.class,id);
+    }
+
+    @Override
+    public void save(Ticket ticket) {
+        LibSQL.insertObject(entityManager,ticket);
+    }
+
+    @Override
+    public void update(Ticket ticket) {
+        //NOP
+    }
+
+    @Override
+    public void delete(Ticket ticket) {
+        LibSQL.deleteObject(entityManager,ticket);
+    }
+}
