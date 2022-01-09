3eea30fa70dc924c2e20267ee8d2f145e83d890d
NathanHayoun <58591332+NathanHayoun@users.noreply.github.com>
2022-01-09 13:29:17 +0100

    Update LibSQL.java

--- sntp-commons/src/main/java/fr/miage/m1/sntp/utils/LibSQL.java
+++ sntp-commons/src/main/java/fr/miage/m1/sntp/utils/LibSQL.java
@@ -14,7 +14,7 @@ import java.util.Map;
 import java.util.Map.Entry;
 
 /**
- * Library that allows eAasy access to the database
+ * Library that allows easy access to the database
  */
 public class LibSQL {
     private static final Logger logger = LoggerFactory.getLogger(LibSQL.class);
