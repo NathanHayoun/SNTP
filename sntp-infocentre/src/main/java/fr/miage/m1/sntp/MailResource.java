a65dc940fcefdd6065e1f6cc8a33d79ecaddee5e
Etan Guir <etan.guir@gmail.com>
2022-01-09 15:01:46 +0100

    Edit: changement de la fonction sendEmail

--- sntp-infocentre/src/main/java/fr/miage/m1/sntp/MailResource.java
+++ sntp-infocentre/src/main/java/fr/miage/m1/sntp/MailResource.java
@@ -1,12 +1,7 @@
 package fr.miage.m1.sntp;
 
-
 import io.quarkus.mailer.Mail;
-import io.quarkus.mailer.reactive.ReactiveMailer;
 import io.quarkus.mailer.Mailer;
-import io.smallrye.common.annotation.Blocking;
-import io.smallrye.mutiny.Uni;
-
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -14,29 +9,18 @@ import javax.ws.rs.Path;
 @Path("/mail")
 public class MailResource {
 
-    @Inject Mailer mailer;
-    @Inject ReactiveMailer reactiveMailer;
+    @Inject
+    Mailer mailer;
 
 
     @GET
-    public void sendEmail() {
+    public void sendEmail(String receiver, String subject, String text) {
         mailer.send(
-                Mail.withText("etan.guir@gmail.com",
-                        "Ahoy from Quarkus",
-                        "A simple email sent from a Quarkus application."
+                Mail.withText(receiver,
+                        subject,
+                        text
                 )
         );
     }
-
-//    @GET
-//    @Path("/reactive")
-//    public Uni<Void> sendEmailUsingReactiveMailer() {
-//        return reactiveMailer.send(
-//                Mail.withText("etan.guir@gmail.com",
-//                        "Ahoy from Quarkus",
-//                        "A simple email sent from a Quarkus application using the reactive API."
-//                )
-//        );
-//    }
 
 }
