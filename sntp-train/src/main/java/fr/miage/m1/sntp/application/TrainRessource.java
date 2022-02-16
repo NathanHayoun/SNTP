5474e57b1531ee11064fe7e3ccca1f7e0be4ef1e
Nathan Hayoun <nathan.hayoun@outlook.fr>
2022-02-16 20:14:13 +0100

    add interface graphique train

---
+++ sntp-train/src/main/java/fr/miage/m1/sntp/application/TrainRessource.java
@@ -0,0 +1,43 @@
+package fr.miage.m1.sntp.application;
+
+import fr.miage.m1.sntp.dto.ArretDTO;
+import fr.miage.m1.sntp.ressources.ArretService;
+import io.quarkus.qute.Template;
+import io.quarkus.qute.TemplateInstance;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@Path("main")
+public class TrainRessource {
+    private static final String PATH_MAIN = "depart";
+    private static final String ARRETS = "arrets";
+
+    @Inject
+    Template main;
+
+    @Inject
+    @RestClient
+    ArretService arretService;
+
+    @ConfigProperty(name = "fr.miage.m1.sntp.numero.train", defaultValue = "2587")
+    int numeroDeTrain;
+
+    /***
+     * @return the template with the arrets of trains that must stop
+     */
+    @Path(PATH_MAIN)
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public TemplateInstance getArrets() {
+        List<ArretDTO> arrets = arretService.getArrets(numeroDeTrain);
+
+        return main.data(ARRETS, arrets);
+    }
+}
