aa8625a87651e7d4b54009b8bc63a1208589f12c
Quentin <quentin095.vaillant@gmail.com>
2022-02-17 17:58:02 +0100

    add reservation gateway

--- sntp-resa/src/main/java/fr/miage/m1/sntp/camel/CamelRoutes.java
+++ sntp-resa/src/main/java/fr/miage/m1/sntp/camel/CamelRoutes.java
@@ -1,6 +1,7 @@
 package fr.miage.m1.sntp.camel;
 
 
+import fr.miage.m1.sntp.models.Reservation;
 import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
@@ -26,12 +27,9 @@ public class CamelRoutes extends RouteBuilder {
 
         camelContext.setTracing(true);
 
-
-        /** from("jms:" + jmsPrefix + "booking?exchangePattern=InOut")//
-         .log("reservation received: ${in.headers}")//
-         .unmarshal().json(ReservationDTO.class)//
-         .bean(reservationHandler, "book").marshal().json()
-         ;
-         **/
+        from("jms:" + jmsPrefix + "booking?exchangePattern=InOut")
+         .log("reservation received: ${in.headers}")
+         .unmarshal().json(Reservation.class)
+         .bean(reservationHandler, "book").marshal().json();
     }
 }
--- sntp-resa/src/main/java/fr/miage/m1/sntp/camel/ReservationGateway.java
+++ sntp-resa/src/main/java/fr/miage/m1/sntp/camel/ReservationGateway.java
@@ -1,6 +1,6 @@
 package fr.miage.m1.sntp.camel;
 
-import fr.miage.m1.sntp.dto.ReservationDTO;
+import fr.miage.m1.sntp.models.Reservation;
 import fr.miage.m1.sntp.services.ReservationService;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -12,8 +12,8 @@ public class ReservationGateway {
     @Inject
     ReservationService bookingService;
 
-    public ReservationDTO book(ReservationDTO reservationRequest) {
-        return null;
+    public Reservation book(Reservation reservationRequest) {
+        return bookingService.createReservation(reservationRequest);
     }
 
 
